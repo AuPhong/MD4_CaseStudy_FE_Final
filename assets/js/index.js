6402a7c80faa7c1b55450b92a2bdf1f947e2ee1f
Trần Hạ Lan <halan4444b@gmail.com>
2022-06-25 17:59:25 +0700

    long 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,12 @@
 $(function() {
     "use strict";
 
+	let arr = [[],[]];
+	for(let i=1; i<13;i++) {
+		chart1(sessionStorage.getItem('id'),i) ;
+		arr[0].push([moneyFlow[0]/1000])
+		arr[1].push([moneyFlow[1]/1000])
+	}
      // chart 1
 	 
 		  var ctx = document.getElementById('chart1').getContext('2d');
@@ -8,17 +14,21 @@ $(function() {
 			var myChart = new Chart(ctx, {
 				type: 'line',
 				data: {
-					labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+					labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
 					datasets: [{
-						label: 'New Visitor',
-						data: [3, 3, 8, 5, 7, 4, 6, 4, 6, 3],
+						label: 'Outcome Flow',
+						data: [arr[1][0], arr[1][1], arr[1][2],arr[1][3],
+							arr[1][4], arr[1][5], arr[1][6], arr[1][7],
+							arr[1][8], arr[1][9], arr[1][10], arr[1][11]],
 						backgroundColor: '#fff',
 						borderColor: "transparent",
 						pointRadius :"0",
 						borderWidth: 3
 					}, {
-						label: 'Old Visitor',
-						data: [7, 5, 14, 7, 12, 6, 10, 6, 11, 5],
+						label: 'Income Flow',
+						data: [arr[0][0], arr[0][1], arr[0][2],arr[0][3],
+							arr[0][4], arr[0][5], arr[0][6], arr[0][7],
+							arr[0][8], arr[0][9], arr[0][10], arr[0][11]],
 						backgroundColor: "rgba(255, 255, 255, 0.25)",
 						borderColor: "transparent",
 						pointRadius :"0",
